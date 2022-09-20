5af8a8468a9942bceb6246c9c6882510c7e30e18
suntoes <mar26xd@gmail.com>
2022-09-20 18:14:06 +0800

    05 - new feature

--- components/chakra.js
+++ components/chakra.js
@@ -18,6 +18,24 @@ export default function Chakra({ cookies, children }) {
   )
 }
 
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
 export async function getServerSideProps({ req }) {
   const mainIgFeedRequest = await fetch(
     'https://coffee-dojo-api-v1.herokuapp.com/api/ig/main'
@@ -27,13 +45,14 @@ export async function getServerSideProps({ req }) {
   )
 
   const { recentPosts } = (await mainIgFeedRequest.json()) || []
-  const branchesData = (await branchesDataRequest.json()) || {}
+  const initialBranchesData = (await branchesDataRequest.json()) || []
 
-  const EnsurePostCount = [...recentPosts.slice(0, 12), ...Array(12 - recentPosts.slice(0, 12).length).fill(1)]
+  const branchesData = shuffle(initialBranchesData)
 
+  const ensurePostCount = [...recentPosts.slice(0, 12), ...Array(12 - recentPosts.slice(0, 12).length).fill(1)]
   const mainIgFeed = []
-  for (let i = 0; i < EnsurePostCount.length; i++) {
-    mainIgFeed.push(EnsurePostCount[i])
+  for (let i = 0; i < ensurePostCount.length; i++) {
+    mainIgFeed.push(ensurePostCount[i])
     if ((i + 1) % 3 === 0) mainIgFeed.push(0)
   }
 
